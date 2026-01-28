c5ec95a4a2992bd5494747821cb23ebae137a56e
Ingvard <ingvard777333@gmail.com>
2026-01-28 17:14:25 +0300

    First

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int cost = 13676;
+        int conversion = 20;
+        int miles;
+        miles = cost / conversion;
+        System.out.println("при оплате " + cost + " р. вы получаете " + miles + " бонусные мили");
 
     }
 }
